3f87ce1014a56297daef7561b7edfda9f7ecb946
Will Knez <wibknez@salud.unm.edu>
2019-12-28 10:47:59 -0700

    Add skeleton for application entry point.

---
+++ src/breakdb/__main__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+The main driver for the fracture (break) detection database project.
+"""
+import sys
+
+
+def main():
+    """
+    The application entry point.
+
+    :return: An exit code.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    sys.exit(main())
