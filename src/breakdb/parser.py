842897f9e0ef7752f1de7bab0eaa9bc9ca0ad9bf
Will Knez <wibknez@salud.unm.edu>
2020-01-02 16:36:07 -0700

    Add skeleton function to parse entire DICOM dataset.

--- src/breakdb/parser.py
+++ src/breakdb/parser.py
@@ -22,7 +22,7 @@ def _get_tag_value(ds, tag):
     :param ds: The dataset to read.
     :param tag: The tag to find.
     :return: The value associated with a tag.
-    :raise MissingTag: If the requested tag could not be found.
+    :raises MissingTag: If the requested tag could not be found.
     """
     if tag.value not in ds:
         raise MissingTag(tag)
@@ -33,18 +33,34 @@ def _get_tag_value(ds, tag):
 def parse_common(ds):
     """
     Parses and returns a dictionary of the values of all common tags required
-    to be in a DICOM file for this project.
+    to be in a DICOM dataset for this project.
 
-    Please note that for a DICOM file that references another, the series
+    Please note that for a DICOM dataset that references another, the series
     identifier will be replaced to match the one contained in the referred
     file.  This function does not perform that operation.
 
-    :param ds: The dataset to read.
+    :param ds: The dataset to search.
     :return: A dictionary of common tag values.
-    :raise MissingTag: If one or more tags could not be found.
+    :raises MissingTag: If one or more tags could not be found.
     """
     return {
         _get_tag_value(ds, CommonTag.SOP_CLASS),
         _get_tag_value(ds, CommonTag.SOP_INSTANCE),
         _get_tag_value(ds, CommonTag.SERIES)
     }
+
+
+def parse_dataset(ds):
+    """
+    Searches for, parses, and returns a dictionary of the values of as many
+    tags as possible available in the specified DICOM dataset.
+
+    :param ds: The dataset to search.
+    :return: A dictionary of available tag values.
+    :raises MissingTag: If one or more expected tags could not be found.
+    """
+    tags = {}
+
+    tags += parse_common(ds)
+
+    return tags
