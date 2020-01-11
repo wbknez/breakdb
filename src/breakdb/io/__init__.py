eae9b1a197de41968a96429ee3b7188c39eb053d
Will Knez <wibknez@salud.unm.edu>
2020-01-11 16:28:18 -0700

    Add functions to read and write databases based on file name(s).

--- src/breakdb/io/__init__.py
+++ src/breakdb/io/__init__.py
@@ -3,6 +3,24 @@ Contains classes and functions pertaining to database serialization.
 """
 import os
 
+from breakdb.io.reading import CsvDatabaseReader, ExcelDatabaseReader, \
+    JsonDatabaseReader
+from breakdb.io.writing import CsvDatabaseWriter, ExcelDatabaseWriter, \
+    JsonDatabaseWriter
+
+_READERS = {
+    ".csv": CsvDatabaseReader(),
+    ".xlsx": ExcelDatabaseReader(),
+    ".json": JsonDatabaseReader()
+}
+
+
+_WRITERS = {
+    ".csv": CsvDatabaseWriter(),
+    ".xlsx": ExcelDatabaseWriter(),
+    ".json": JsonDatabaseWriter()
+}
+
 
 def filter_files(paths, extensions=None, relative=False):
     """
@@ -28,3 +46,39 @@ def filter_files(paths, extensions=None, relative=False):
                 for extension in extensions:
                     if file.endswith(extension):
                         yield os.path.join(root, file)
+
+
+def read_database(file_path):
+    """
+    Reads a database located from the specified file on disk.
+
+    :param file_path: The file to read a database from.
+    :return: A database.
+    :raises KeyError: If a reader cannot be found for a particular file path.
+    """
+    _, extension = os.path.splitext(file_path)
+
+    if extension not in _READERS:
+        raise KeyError(f"Cannot read database - unknown file extension:"
+                       f" {file_path}.")
+
+    with open(file_path, "r") as stream:
+        return _READERS[extension].read(stream)
+
+
+def write_database(db, file_path):
+    """
+    Writes the specified database to the specified file on disk.
+
+    :param db: The database to write.
+    :param file_path: The file to write the database to.
+    :raises KeyError: If a writer cannot be found for a particular file path.
+    """
+    _, extension = os.path.splitext(file_path)
+
+    if extension not in _WRITERS:
+        raise KeyError(f"Cannot write database - unknown file extension: "
+                       f"{file_path}")
+
+    with open(file_path, "w") as stream:
+        _WRITERS[extension].write(db, stream)
