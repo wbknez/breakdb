e63e1dc37cbc7b15891789e428458f9fc4b7548b
Will Knez <wibknez@salud.unm.edu>
2020-01-13 11:56:19 -0700

    Add function to create a database from one or more user-specified directories.

--- src/breakdb/action.py
+++ src/breakdb/action.py
@@ -2,10 +2,17 @@
 Contains classes and functions related to all actions that may be invoked
 via the command line.
 """
+import logging
 from enum import IntEnum
+from functools import partial
+from multiprocessing.pool import Pool
 
+import pandas as pd
 from pydicom import dcmread
 
+from breakdb.io import filter_files, COLUMN_NAMES, write_database
+from breakdb.merge import organize_parsed, merge_dicom
+from breakdb.parse import parse_dicom
 from breakdb.util import format_dataset
 
 
@@ -25,6 +32,62 @@ class ExitCode(IntEnum):
     """
 
 
+def create_database(args):
+    """
+    Creates a Pandas dataframe from DICOM files found by searching one or
+    more user-specified directories.
+
+    :param args: The user-chosen options to use.
+    :return: An exit code (0 if success, otherwise 1).
+    """
+    logger = logging.getLogger(__name__)
+
+    parser = partial(parse_dicom, skip_broken=args.skip_broken)
+    merger = partial(merge_dicom, skip_broken=args.skip_broken)
+
+    try:
+        with Pool(processes=args.parallel) as pool:
+            logger.info("Searching directories for DICOM files: {}.",
+                        args.PATHS)
+            logger.info("Parsing DICOM files...")
+
+            parsed = pool.map(parser, filter_files(args.PATHS,
+                                                   extensions=".dcm",
+                                                   relative=args.relative))
+            logger.debug("Parsed {} files.", len(parsed))
+            logger.debug("Parsing complete.")
+
+            logger.info("Organizing and merging parsed datasets into single "
+                        "entries...")
+            merged = pool.map(merger, organize_parsed(parsed))
+
+            logger.debug("Merged {} parsed datasets into {} entries.",
+                         len(parsed), len(merged))
+            logger.debug("Merging complete.")
+
+            logger.info("Creating database from entries...")
+            db = pd.DataFrame(merged, columns=COLUMN_NAMES)
+
+            logger.debug("Searching for and removing empty rows.")
+            db.dropna(inplace=True)
+
+            logger.debug("Deleted {} rows.", len(merged) - len(db))
+            logger.debug("Final database size is {} entries.", len(db))
+
+            logger.info("Serializing database to disk...")
+            write_database(db, args.output)
+
+            logger.debug("Wrote to file: {}.", args.output)
+
+            logger.info("Database creation complete.")
+
+        return ExitCode.SUCCESS
+    except Exception as ex:
+        logger.error("Could not create database: {}.", ex)
+
+        return ExitCode.FAILURE
+
+
 def print_tags(args):
     """
     Pretty-prints all tags in a specified file with options.
@@ -35,6 +98,8 @@ def print_tags(args):
     :param args: The user-chosen options to use.
     :return: An exit code (0 if success, otherwise 1).
     """
+    logger = logging.getLogger(__name__)
+
     try:
         ds = dcmread(args.FILE)
 
@@ -43,5 +108,7 @@ def print_tags(args):
                              args.flat, args.use_color))
 
         return ExitCode.SUCCESS
-    except:
+    except Exception as ex:
+        logger.error("Could not read metadata: {}.", ex)
+
         return ExitCode.FAILURE
