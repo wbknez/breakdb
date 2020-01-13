134c762bff5a2825e2ab0f8bcc2869ddfad98519
Will Knez <wibknez@salud.unm.edu>
2020-01-12 22:45:41 -0700

    Refactor filter_files function to I/O module.

--- src/breakdb/util.py
+++ src/breakdb/util.py
@@ -76,31 +76,6 @@ class ColorizingLogFormatter(logging.Formatter):
         return message
 
 
-def filter_files(paths, extensions=None):
-    """
-    Searches for all files in the specified collection of paths and filters
-    them by the specified collection of admissible extensions.
-
-    :param paths: A collection of directories to search.
-    :param extensions: A collection of extensions to filter by.
-    :return: A generator over a collection of filtered files.
-    """
-    if not extensions:
-        raise ValueError("At least one extension must be provided.")
-
-    if isinstance(extensions, str):
-        extensions = [extensions]
-
-    for file_path in paths:
-        for root, _, files in os.walk(file_path, topdown=False):
-            root = os.path.abspath(root)
-
-            for file in files:
-                for extension in extensions:
-                    if file.endswith(extension):
-                        yield os.path.join(root, file)
-
-
 def format_dataset(ds, indent=0, hide_code=False, hide_desc=False,
                    hide_value=False, top_level=False, flat=False,
                    use_color=True):
