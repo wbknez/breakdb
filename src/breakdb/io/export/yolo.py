3a21a3b9ba2343e6b5a4deec42d054c435592459
Will Knez <wibknez@salud.unm.edu>
2020-02-16 11:01:20 -0700

    Add function to create scaled bounding boxes in YOLO format.

---
+++ src/breakdb/io/export/yolo.py
@@ -0,0 +1,24 @@
+"""
+Contains classes and functions pertaining to the creation of YOLOv3 datasets
+from a collated DICOM database.
+"""
+import numpy as np
+
+
+def create_bounding_box(coords, width, height):
+    """
+    Converts the specified collection of coordinates and specified image
+    dimensions into a YOLOv3 compatible bounding box.
+
+    :param coords: The DICOM annotation as a list of single coordinates.
+    :param width: The width of an image.
+    :param height: The height of an image.
+    :return: A YOLOv3 bounding box.
+    """
+    x = np.array([value for value in coords[0::2]], dtype=np.float)
+    y = np.array([value for value in coords[1::2]], dtype=np.float)
+
+    x_ratio = (np.max(x) - np.min(x)) / width
+    y_ratio = (np.max(y) - np.min(y)) / height
+
+    return [x_ratio / 2.0, y_ratio / 2.0, x_ratio, y_ratio]
