32fad9972c71a394276e631229792820bc34bc9f
Will Knez <wibknez@salud.unm.edu>
2020-02-26 22:18:07 -0700

    Update unit tests for transform_coords and transform_coordinate_collection.

--- tests/io/image/test_transform_coordinate_collection.py
+++ tests/io/image/test_transform_coordinate_collection.py
@@ -4,7 +4,8 @@ works correctly.
 """
 import numpy as np
 
-from breakdb.io.image import transform_coordinate_collection
+from breakdb.io.image import transform_coordinate_collection, \
+    compute_resize_transform
 
 
 class TestTransformCoordinateCollection:
@@ -12,7 +13,7 @@ class TestTransformCoordinateCollection:
     Test suite for :function: 'transform_coordinate_collection'.
     """
 
-    def test_transform_coordinate_collection_does_nothing_if_same(self):
+    def test_transform_coordinate_collection_does_nothing_if_unity(self):
         width = np.random.randint(100, 1920)
         height = np.random.randint(100, 1200)
 
@@ -25,37 +26,84 @@ class TestTransformCoordinateCollection:
 
             coords.append([coord for xy in zip(x, y) for coord in xy])
 
-        transformed = transform_coordinate_collection(coords, width, height,
-                                                      width, height)
-        expected = coords
+        origin, ratios = compute_resize_transform(width, height, width,
+                                                  height, width, height)
+        transformed = transform_coordinate_collection(coords, origin, ratios)
 
-        assert transformed == expected
+        assert np.array_equal(transformed, coords)
 
-    def test_transform_coordinate_collection_scales_correctly(self):
+    def test_transform_coordinate_collection_downscales(self):
         width = np.random.randint(100, 1920)
         height = np.random.randint(100, 1200)
-        new_width = np.random.randint(100, 1920)
-        new_height = np.random.randint(100, 1200)
+
+        target_width = np.random.randint(10, width - 1)
+        target_height = np.random.randint(10, height - 1)
+
+        resize_width = np.random.randint(1, target_width - 1)
+        resize_height = np.random.randint(1, target_height - 1)
 
         num_annotations = np.random.randint(1, 10)
         coords = []
+
+        for _ in range(num_annotations):
+            x = np.random.randint(0, width, 5)
+            y = np.random.randint(0, height, 5)
+
+            coords.append([coord for xy in zip(x, y) for coord in xy])
+
+        origin, ratios = compute_resize_transform(width, height,
+                                                  target_width,
+                                                  target_height,
+                                                  resize_width, resize_height)
+        transformed = transform_coordinate_collection(coords, origin, ratios)
         expected = []
 
+        for coord in coords:
+            x_c = np.array(coord[0::2], np.float)
+            y_c = np.array(coord[1::2], np.float)
+
+            expected.append(
+                np.array([c for xy in zip(x_c * ratios[0] + origin[0],
+                                          y_c * ratios[1] + origin[1])
+                          for c in xy])
+            )
+
+        assert np.array_equal(transformed, expected)
+
+    def test_transform_coordinate_collection_upscales(self):
+        width = np.random.randint(100, 1920)
+        height = np.random.randint(100, 1200)
+
+        target_width = np.random.randint(width + 1, 1340)
+        target_height = np.random.randint(height + 1, 2400)
+
+        resize_width = np.random.randint(width + 1, target_width - 1)
+        resize_height = np.random.randint(height + 1, target_height - 1)
+
+        num_annotations = np.random.randint(1, 10)
+        coords = []
+
         for _ in range(num_annotations):
             x = np.random.randint(0, width, 5)
             y = np.random.randint(0, height, 5)
 
             coords.append([coord for xy in zip(x, y) for coord in xy])
 
-            x = x.astype(np.float) * (new_width / width)
-            y = y.astype(np.float) * (new_height / height)
+        origin, ratios = compute_resize_transform(width, height,
+                                                  target_width,
+                                                  target_height,
+                                                  resize_width, resize_height)
+        transformed = transform_coordinate_collection(coords, origin, ratios)
+        expected = []
 
-            expected.append([coord for xy in zip(x, y) for coord in xy])
+        for coord in coords:
+            x_c = np.array(coord[0::2], np.float)
+            y_c = np.array(coord[1::2], np.float)
 
-        transformed = transform_coordinate_collection(coords, width, height,
-                                                      new_width, new_height)
-
-        transformed = np.array(transformed)
-        expected = np.array(expected)
+            expected.append(
+                np.array([c for xy in zip(x_c * ratios[0] + origin[0],
+                                          y_c * ratios[1] + origin[1])
+                          for c in xy])
+            )
 
         assert np.array_equal(transformed, expected)
--- tests/io/image/test_transform_coords.py
+++ tests/io/image/test_transform_coords.py
@@ -4,7 +4,7 @@ coordinates from one image to another after scaling is correct.
 """
 import numpy as np
 
-from breakdb.io.image import transform_coords
+from breakdb.io.image import transform_coords, compute_resize_transform
 
 
 class TestTransformCoords:
@@ -12,67 +12,68 @@ class TestTransformCoords:
     Test suite for :function: 'transform_coords'.
     """
 
-    def test_transform_coords_does_nothing_if_scales_are_the_same(self):
-        width = np.random.randint(1, 1920)
-        height = np.random.randint(1, 1200)
+    def transform_coords_does_nothing_if_unity(self):
+        width = np.random.randint(100, 1920)
+        height = np.random.randint(100, 1200)
 
         x = np.random.randint(0, width, 5)
         y = np.random.randint(0, height, 5)
 
         coords = np.array([c for xy in zip(x, y) for c in xy], np.float)
-        transformed = transform_coords(coords, width, height, width, height)
+
+        origin, ratios = (0.0, 0.0), (1.0, 1.0)
+        transformed = transform_coords(coords, origin, ratios)
 
         assert np.array_equal(transformed, coords)
 
-    def test_transform_coords_applies_new_width_correctly(self):
-        width = np.random.randint(1, 1920)
-        height = np.random.randint(1, 1200)
+    def transform_coords_downscales(self):
+        width = np.random.randint(100, 1920)
+        height = np.random.randint(100, 1200)
 
-        new_width = np.random.randint(1, 1920)
+        target_width = np.random.randint(10, width - 1)
+        target_height = np.random.randint(10, height - 1)
+
+        resize_width = np.random.randint(1, target_width - 1)
+        resize_height = np.random.randint(1, target_height - 1)
 
         x = np.random.randint(0, width, 5)
         y = np.random.randint(0, height, 5)
 
         coords = np.array([c for xy in zip(x, y) for c in xy], np.float)
-        transformed = transform_coords(coords, width, height, new_width,
-                                       height)
-        expected = np.array([c for xy in zip(x * (new_width / width), y)
+
+        origin, ratios = compute_resize_transform(width, height,
+                                                  target_width,
+                                                  target_height,
+                                                  resize_width, resize_height)
+        transformed = transform_coords(coords, origin, ratios)
+        expected = np.array([c for xy in zip(x * ratios[0] + origin[0],
+                                             y * ratios[1] + origin[1])
                              for c in xy])
 
         assert np.array_equal(transformed, expected)
 
-    def test_transform_coords_applies_new_height_correctly(self):
-        width = np.random.randint(1, 1920)
-        height = np.random.randint(1, 1200)
+    def transform_coords_upscales(self):
+        width = np.random.randint(100, 1920)
+        height = np.random.randint(100, 1200)
 
-        new_height = np.random.randint(1, 1920)
+        target_width = np.random.randint(width + 2, 3840)
+        target_height = np.random.randint(height + 2, 2400)
 
-        x = np.random.randint(0, width, 5)
-        y = np.random.randint(0, height, 5)
-
-        coords = np.array([c for xy in zip(x, y) for c in xy], np.float)
-        transformed = transform_coords(coords, width, height, width,
-                                       new_height)
-        expected = np.array([c for xy in zip(x, y * (new_height / height))
-                             for c in xy])
-
-        assert np.array_equal(transformed, expected)
-
-    def test_transform_coords_applies_new_width_and_height_correctly(self):
-        width = np.random.randint(1, 1920)
-        height = np.random.randint(1, 1200)
-
-        new_width = np.random.randint(1, 1920)
-        new_height = np.random.randint(1, 1200)
+        resize_width = np.random.randint(width + 1, target_width - 1)
+        resize_height = np.random.randint(height + 1, target_height - 1)
 
         x = np.random.randint(0, width, 5)
         y = np.random.randint(0, height, 5)
 
         coords = np.array([c for xy in zip(x, y) for c in xy], np.float)
-        transformed = transform_coords(coords, width, height, new_width,
-                                       new_height)
-        expected = np.array([c for xy in zip(x * (new_width / width),
-                                             y * (new_height / height))
+
+        origin, ratios = compute_resize_transform(width, height,
+                                                  target_width,
+                                                  target_height,
+                                                  resize_width, resize_height)
+        transformed = transform_coords(coords, origin, ratios)
+        expected = np.array([c for xy in zip(x * ratios[0] + origin[0],
+                                             y * ratios[1] + origin[1])
                              for c in xy])
 
         assert np.array_equal(transformed, expected)
