50800e26e37db0fa242bf333e3a02559d9693f0b
Will Knez <wibknez@salud.unm.edu>
2020-03-05 17:21:00 -0700

    Fix exportation API.

--- src/breakdb/io/export/__init__.py
+++ src/breakdb/io/export/__init__.py
@@ -1,6 +1,8 @@
 """
 
 """
+import os
+import shutil
 from abc import ABCMeta, abstractmethod
 
 
@@ -14,27 +16,53 @@ class ExportEntryFormatError(Exception):
     format.
     """
 
-    def __init__(self, index, file_path, export_type):
-        super().__init__(f"Could not format row {index} as {export_type} "
-                         f"with image: {file_path}.")
+    def __init__(self, name, export_type):
+        super().__init__(f"Could not format entry: {name} as: {export_type}.")
 
 
-class AnnotationExporter(metaclass=ABCMeta):
+class DatabaseEntryExporter(metaclass=ABCMeta):
     """
-    Represents a mechanism for exporting a collated DICOM database to a
-    file-based annotation structure.
+    Represents a mechanism for exporting a single entry from a collated DICOM
+    database to a file-based annotation.
     """
 
     @abstractmethod
-    def create_bounding_box(self, coords, width, height):
+    def create_directory_structure(self, base_dir, force=False):
         """
-        Converts the specified collection of coordinates and specified image
-        dimensions into a format compatible bounding box.
+        Creates the directory structure expected for a custom formatted
+        dataset.
 
-        :param coords: The DICOM annotation as a list of single coordinates.
-        :param width: The maximum bounding width.
-        :param height: The maximum bounding height.
-        :return: A formatted bounding box.
+        :param base_dir: The directory in which to create the directory
+        structure.
+        :param force: Whether or not to overwrite a directory if it already
+        exists.
+        :return: A tuple containing the annotation, image, and master list
+        directory paths.
+        """
+        pass
+
+    @abstractmethod
+    def export(self, ds, name, base_dir, target_width=None, target_height=None,
+               ignore_scaling=False, ignore_windowing=True,
+               keep_aspect_ratio=True, no_upscale=False, skip_broken=False):
+        """
+        Exports the specified database entry
+
+        :param ds: The DICOM dataset to use.
+        :param name: The base name to use for exportation.
+        :param base_dir: The directory in which to export the entry.
+        :param target_width: The maximum width to resize the image to.
+        :param target_height: The maximum height to resize the image to.
+        :param ignore_scaling: Whether or not to ignore, or not apply,
+        any applicable scaling operations.
+        :param ignore_windowing: Whether or not to ignore, or not apply,
+        any applicable windowing operations.
+        :param keep_aspect_ratio: Whether or not to ensure the aspect ratio
+        stays the same during (any) resize operations.
+        :param no_upscale: Whether or not to forbid upscaling.
+        :param skip_broken: Whether or not to ignore I/O errors.
+        :return: A tuple containing the master list identifier and
+        classification.
         """
         pass
 
@@ -46,7 +74,7 @@ def export_image(ds, file_path, target_width=None, target_height=None,
     Exports an image from the specified dataset with the specified parameters.
 
     :param ds: The DICOM dataset to use.
-    :param file_path: The
+    :param file_path: The file to save the image data to.
     :param target_width: The maximum width to resize the image to.
     :param target_height: The maximum height to resize the image to.
     :param ignore_scaling: Whether or not to ignore, or not apply,
@@ -56,7 +84,8 @@ def export_image(ds, file_path, target_width=None, target_height=None,
     :param keep_aspect_ratio: Whether or not to ensure the aspect ratio
     stays the same during (any) resize operations.
     :param no_upscale: Whether or not to forbid upscaling.
-    :return: A transformation for coordinate conversion.
+    :return: A tuple containing the exported image dimensions and
+    transformation for coordinate conversion.
     """
     attrs, arr = read_from_dataset(ds, ignore_scaling=ignore_scaling,
                                    ignore_windowing=ignore_windowing)
@@ -65,3 +94,28 @@ def export_image(ds, file_path, target_width=None, target_height=None,
                                  no_upscale)
 
     image.save(file_path)
+
+    return (image.width, image.height), transform
+
+
+def make_directory(dir_path, force=False):
+    """
+    Creates the specified directory if it does not exist, otherwise
+    overwrites it if the specified flag is set.
+
+    If the directory exists and the flag is not set then an exception will
+    be raised.
+
+    :param dir_path: The path to the directory to create.
+    :param force: Whether or not to overwrite a directory if it already exists.
+    :raises FileExistsError: If a directory alreadt exists and the force
+    flag is not set.
+    """
+    try:
+        os.mkdir(dir_path)
+    except FileExistsError:
+        if force:
+            shutil.rmtree(dir_path)
+            os.mkdir(dir_path)
+        else:
+            raise
